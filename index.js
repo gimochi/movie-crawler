124cf5d031e6b833539f69da6c8fb9030dabb982
kms <minsubkim94@gmail.com>
2017-07-28 16:50:17 +0900

    Add line messaging

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 var phantom = require('phantom');
+const line = require('@line/bot-sdk');
+const config = require('./config.json');
+const Client = require('@line/bot-sdk').Client;
+
+const client = new Client(config.config);
+
 
 
 var phInstance = null;
 
 var date = 20170731;
+var url = "http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&theatercode=0013&date="+date+"&screencodes=&screenratingcode=&regioncode=";
 phantom.create(['--ignore-ssl-errors=yes', '--load-images=no'])
        .then(instance => {
         phInstance = instance;
@@ -11,8 +18,8 @@ phantom.create(['--ignore-ssl-errors=yes', '--load-images=no'])
         })
        .then(page => {
            console.log("하이");
-           
-           page.open("http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&theatercode=0013&date="+date+"&screencodes=&screenratingcode=&regioncode=").then( status => {
+               
+           page.open(url).then( status => {
             console.log("결과"+status);
             if(status){
                 
@@ -25,7 +32,7 @@ phantom.create(['--ignore-ssl-errors=yes', '--load-images=no'])
                         if(html[i].textContent.search("덩케르크") != -1){
                             console.log("있당");
                             //TODO line messaging
-                            
+                            client.pushMessage(config.userId, {type: 'text', text: date+'의 덩케르트가 떴땅 \n 주소는 ' + url });
                         }else{
                             console.log("없당");
                             
