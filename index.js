0693806593d34dd11f2626fefd2ed3642a705dbb
kms <minsubkim94@gmail.com>
2017-08-01 12:23:57 +0900

    Delete console log

--- index.js
+++ index.js
@@ -9,39 +9,38 @@ const client = new Client(config.config);
 
 var phInstance = null;
 
-var date = 20170731;
+var date = 20170803;
 var url = "http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&theatercode=0013&date="+date+"&screencodes=&screenratingcode=&regioncode=";
+var isFinished = false;
 phantom.create(['--ignore-ssl-errors=yes', '--load-images=no'])
        .then(instance => {
         phInstance = instance;
         return instance.createPage();
         })
        .then(page => {
-           console.log("하이");
-               
-           page.open(url).then( status => {
-            console.log("결과"+status);
-            if(status){
-                
-                page.evaluate(function() {
-                    return document.getElementsByClassName('sect-showtimes');
-                }).then(function(html){
-                    console.log("개수"+html.length);
+           
+           var doing = setInterval(function(){
+            page.open(url).then( status => {
+              
+                if(status){
                     
-                    for(var i=0; i<html.length;i++){
-                        if(html[i].textContent.search("덩케르크") != -1){
-                            console.log("있당");
-                            //TODO line messaging
-                            client.pushMessage(config.userId, {type: 'text', text: date+'의 덩케르트가 떴땅 \n 주소는 ' + url });
-                        }else{
-                            console.log("없당");
-                            
+                    page.evaluate(function() {
+                        return document.getElementsByClassName('sect-showtimes');
+                    }).then(function(html){
+                                
+                        for(var i=0; i<html.length;i++){
+                            if(html[i].textContent.search("덩케르크") != -1){
+                                client.pushMessage(config.userId, {type: 'text', text: date+'의 덩케르트가 떴땅 \n 주소는 ' + url });
+                                clearInterval(doing);
+                                phInstance.exit();
+                            }
                         }
-                    }
 
-                    phInstance.exit();
-                });
-            }
-           })
-          
+                       
+                    });
+                }
+
+            })
+           }, 100000);
+ 
        })
